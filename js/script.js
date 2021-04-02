b026921669e7fd59a04f62b31d8e522057cadf05
Roman <rr-13@ya.ru>
2021-04-02 15:47:11 +0800

    Section form

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
   $('.comment__slider').slick({
-    infinite: true,
+    centerMode: true,
+    centerPadding: '60px',
     slidesToShow: 1,
-    slidesToScroll: 1,
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ],
     prevArrow: '<button type="button" class="slick-prev"><img src="img/left__arrow.png" alt=""/></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="img/right__arrow.png" alt=""/></button>', 
   });
